6a45f80a0ab6eeb921ba56ee5df4c767b0faca10
mughees605 <mughees605@gmail.com>
2017-10-19 16:55:36 -0700

    small change

--- family-traacking-app/src/pages/groups/groups.ts
+++ family-traacking-app/src/pages/groups/groups.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams, Events, LoadingController } from 'ionic-angular';
 import { NewgroupsPage } from '../newgroups/newgroups';
 import { GroupService  } from '../../providers/groups/groups.service';
@@ -6,14 +6,14 @@ import { GroupService  } from '../../providers/groups/groups.service';
   selector: 'page-groups',
   templateUrl: 'groups.html',
 })
-export class GroupsPage {
+export class GroupsPage implements OnInit{
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events,
     public loadingCtrl: LoadingController, public groupService:GroupService) {
   }
   allMyGroups = []
   
-  ionViewWillEnter() {
+  ngOnInit(){
     let loader = this.loadingCtrl.create({
       content: 'Getting your groups, Please wait...'
     });
@@ -23,6 +23,9 @@ export class GroupsPage {
     this.events.subscribe('newgroup', () => {
       this.allMyGroups = this.groupService.myGroups;
     })
+  }
+  ionViewWillEnter() {
+    
   }
 
   ionViewDidLeave() {
