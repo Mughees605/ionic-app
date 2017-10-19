078318c7d2d3ad424e4b15479d44397fef2f1578
mughees605 <mughees605@gmail.com>
2017-10-19 15:29:24 -0700

    groups

--- family-traacking-app/src/pages/groups/groups.ts
+++ family-traacking-app/src/pages/groups/groups.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Events, LoadingController } from 'ionic-angular';
 import { NewgroupsPage } from '../newgroups/newgroups';
+import { GroupService  } from '../../providers/groups/groups.service';
 @Component({
   selector: 'page-groups',
   templateUrl: 'groups.html',
 })
 export class GroupsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events,
+    public loadingCtrl: LoadingController, public groupService:GroupService) {
+  }
+  allMyGroups = []
+  
+  ionViewWillEnter() {
+    let loader = this.loadingCtrl.create({
+      content: 'Getting your groups, Please wait...'
+    });
+    loader.present();
+    this.groupService.getmygroups();
+    loader.dismiss();
+    this.events.subscribe('newgroup', () => {
+      this.allMyGroups = this.groupService.myGroups;
+    })
   }
 
+  ionViewDidLeave() {
+    this.events.unsubscribe('newgroup');
+  }
   addGroup(){
     this.navCtrl.push(NewgroupsPage);
   }
--- family-traacking-app/src/providers/groups/groups.service.ts
+++ family-traacking-app/src/providers/groups/groups.service.ts
@@ -3,14 +3,18 @@ import 'rxjs/Rx';
 import { Subject } from 'rxjs/Subject';
 import { AngularFireAuth } from 'angularfire2/auth';
 import firebase from 'firebase';
+import { Events } from 'ionic-angular';
+
 @Injectable()
 
 export class GroupService{
     firegroup = firebase.database().ref('/groups');
+    myGroups: Array<any> = [];
     
-    constructor(){
+    constructor(public events:Events){
 
     }
+    
 
     addgroup(newGroup) {
         var promise = new Promise((resolve, reject) => {
@@ -25,5 +29,25 @@ export class GroupService{
           })
         });
         return promise;
+      } 
+
+      getmygroups() {
+          let uid = localStorage.getItem('uid')
+        this.firegroup.child(uid).once('value', (snapshot) => {
+          this.myGroups = [];
+          if(snapshot.val() != null) {
+            var temp = snapshot.val();
+            for (var key in temp) {
+              var newgroup = {
+                groupName: key,
+                groupimage: temp[key].groupimage
+              }
+              this.myGroups.push(newgroup);
+            }
+          }
+          this.events.publish('newgroup');
+        })
+        
       }
+    
 }
